4a18270b22dfafc1d34927cfe0fdfed2811e3d97
Kun Lai <me@imlk.top>
2024-02-04 23:16:37 +0800

    serialize as cbor file and recover data from Montgomery form
    Signed-off-by: Kun Lai <me@imlk.top>

--- std/utils/export_utils/export_utils.go
+++ std/utils/export_utils/export_utils.go
@@ -1,40 +1,24 @@
 package export_utils
 
 import (
-	"encoding/binary"
-	"encoding/json"
+	"math/big"
 	"os"
-	"strconv"
 
 	"github.com/consensys/gnark-crypto/ecc/bls12-377/fr"
-	"github.com/consensys/gnark-crypto/field/pool"
 	"github.com/consensys/gnark/constraint"
 	cs "github.com/consensys/gnark/constraint/bls12-377"
 	"github.com/consensys/gnark/std/rangecheck/varuna"
+
+	"github.com/fxamacker/cbor/v2"
 )
 
-type FrElement struct {
-	fr.Element
-}
-
-func (z FrElement) MarshalJSON() ([]byte, error) {
-	var b [32]byte
-	binary.BigEndian.PutUint64(b[24:32], z.Element[0])
-	binary.BigEndian.PutUint64(b[16:24], z.Element[1])
-	binary.BigEndian.PutUint64(b[8:16], z.Element[2])
-	binary.BigEndian.PutUint64(b[0:8], z.Element[3])
-
-	vv := pool.BigInt.Get()
-	vv.SetBytes(b[:])
-	bs := []byte("\"" + vv.Text(10) + "\"")
-	pool.BigInt.Put(vv)
-	return bs, nil
-}
+/* A little-endian [4]uint64 array for each bls12-377 scale field variable. Note that fr.Element is in Montgomery form and should be convert to normal form with .Bits(). */
+type Element [4]uint64
 
 type ConstraintRaw struct {
-	A map[int]FrElement `json:"a"`
-	B map[int]FrElement `json:"b"`
-	C map[int]FrElement `json:"c"`
+	A map[int]Element `json:"a"`
+	B map[int]Element `json:"b"`
+	C map[int]Element `json:"c"`
 }
 type R1CSRaw []ConstraintRaw
 
@@ -42,31 +26,49 @@ func SerializeR1CS(r1cs constraint.R1CS, filePath string) error {
 	r1csRaw := make(R1CSRaw, 0, r1cs.GetNbConstraints())
 
 	for _, r1c := range r1cs.GetR1Cs() {
-		c := ConstraintRaw{make(map[int]FrElement, len(r1c.L)), make(map[int]FrElement, len(r1c.R)), make(map[int]FrElement, len(r1c.O))}
+		c := ConstraintRaw{make(map[int]Element, len(r1c.L)), make(map[int]Element, len(r1c.R)), make(map[int]Element, len(r1c.O))}
 		for _, term := range r1c.L {
 			e := r1cs.GetCoefficient(int(term.CID))
 			var ee fr.Element
 			copy(ee[:], e[:4])
-			c.A[int(term.VID)] = FrElement{ee}
+			c.A[int(term.VID)] = ee.Bits()
+
+			var bi big.Int
+			ee.BigInt(&bi)
+			if bi.Cmp(fr.Modulus()) != -1 {
+				panic("wft!!!")
+			}
 		}
 		for _, term := range r1c.R {
 			e := r1cs.GetCoefficient(int(term.CID))
 			var ee fr.Element
 			copy(ee[:], e[:4])
-			c.B[int(term.VID)] = FrElement{ee}
+			c.B[int(term.VID)] = ee.Bits()
+
+			var bi big.Int
+			ee.BigInt(&bi)
+			if bi.Cmp(fr.Modulus()) != -1 {
+				panic("wft!!!")
+			}
 		}
 		for _, term := range r1c.O {
 			e := r1cs.GetCoefficient(int(term.CID))
 			var ee fr.Element
 			copy(ee[:], e[:4])
-			c.C[int(term.VID)] = FrElement{ee}
+			c.C[int(term.VID)] = ee.Bits()
+
+			var bi big.Int
+			ee.BigInt(&bi)
+			if bi.Cmp(fr.Modulus()) != -1 {
+				panic("wft!!!")
+			}
 		}
 		r1csRaw = append(r1csRaw, c)
 	}
 
 	{
 		fR1CS, _ := os.Create(filePath)
-		got, err := json.MarshalIndent(&r1csRaw, "", "\t") /* @imlk: this line cost 7GB ram */
+		got, err := cbor.Marshal(&r1csRaw)
 		if err != nil {
 			return err
 		}
@@ -79,23 +81,23 @@ func SerializeR1CS(r1cs constraint.R1CS, filePath string) error {
 }
 
 type AssignmentRaw struct {
-	Variables          []FrElement `json:"variables"`
-	PrimaryInputSize   uint        `json:"primary_input_size"`
-	AuxiliaryInputSize uint        `json:"auxiliary_input_size"`
+	Variables          []Element `json:"variables"`
+	PrimaryInputSize   uint      `json:"primary_input_size"`
+	AuxiliaryInputSize uint      `json:"auxiliary_input_size"`
 }
 
 // TODO: primary_input_size and auxiliary_input_size are actually not used
 
 func SerializeAssignment(solution *cs.R1CSSolution, filePath string) error {
-	assignmentRaw := AssignmentRaw{make([]FrElement, 0, len(solution.W)), 0, 0}
+	assignmentRaw := AssignmentRaw{make([]Element, 0, len(solution.W)), 0, 0}
 
 	for _, v := range solution.W {
-		assignmentRaw.Variables = append(assignmentRaw.Variables, FrElement{v})
+		assignmentRaw.Variables = append(assignmentRaw.Variables, Element(v.Bits()))
 	}
 
 	{
 		fAssignment, _ := os.Create(filePath)
-		got, err := json.MarshalIndent(&assignmentRaw, "", "\t")
+		got, err := cbor.Marshal(&assignmentRaw)
 		if err != nil {
 			return err
 		}
@@ -108,25 +110,24 @@ func SerializeAssignment(solution *cs.R1CSSolution, filePath string) error {
 }
 
 type LookupRaw struct {
-	Table       [][3]string     `json:"table"`
+	Table       [][3]uint32     `json:"table"` /* Note the type of value is uint32, in case the baseLength shall not larger than 32 */
 	Constraints []ConstraintRaw `json:"constraints"`
 }
 
 func SerializeLookup(lookup *varuna.Lookup, r1cs constraint.R1CS, filePath string) error {
-	lookupRaw := LookupRaw{make([][3]string, 0, lookup.NbTable), make([]ConstraintRaw, 0, len(lookup.A))}
+	lookupRaw := LookupRaw{make([][3]uint32, 0, lookup.NbTable), make([]ConstraintRaw, 0, len(lookup.A))}
 	for i := 0; i < lookup.NbTable; i++ {
-		lookupRaw.Table = append(lookupRaw.Table, [3]string{strconv.Itoa(i), "0", "0"})
+		lookupRaw.Table = append(lookupRaw.Table, [3]uint32{uint32(i), 0, 0})
 	}
 
 	for _, lc := range lookup.A {
-		c := ConstraintRaw{make(map[int]FrElement, len(lc)), make(map[int]FrElement, 0), make(map[int]FrElement, 0)}
+		c := ConstraintRaw{make(map[int]Element, len(lc)), make(map[int]Element, 0), make(map[int]Element, 0)}
 		for _, term := range lc {
 			e := r1cs.GetCoefficient(int(term.CID))
 			var ee fr.Element
 			copy(ee[:], e[:4])
-			c.A[int(term.VID)] = FrElement{ee}
+			c.A[int(term.VID)] = ee.Bits()
 		}
-		// TODO: debug term.VID is 4294967295
 		/* no need to set B and C since they are all zeros */
 		// c.B[0] = FrElement{} /* 0 */
 		// c.C[0] = FrElement{} /* 0 */
@@ -135,7 +136,7 @@ func SerializeLookup(lookup *varuna.Lookup, r1cs constraint.R1CS, filePath strin
 
 	{
 		fLookup, _ := os.Create(filePath)
-		got, err := json.MarshalIndent(&lookupRaw, "", "\t")
+		got, err := cbor.Marshal(&lookupRaw)
 		if err != nil {
 			return err
 		}
